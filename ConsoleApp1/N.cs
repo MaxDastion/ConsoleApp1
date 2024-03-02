0125f1c6d079fc934f0c22d405259139d9562a3a
User <User@DESKTOP-3V2P9PB>
2024-03-02 08:01:46 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/N.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class TempClass
+    {
+     public TempClass () {
+            Console.Write("Enter first number: ");
+            int temp1 = Convert.ToInt32( Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int temp2 = Convert.ToInt32(Console.ReadLine());
+            if (temp1 == temp2)
+            {
+                Console.WriteLine("The numbers are thre same");
+            }
+            else if (temp1 > temp2) {
+                Console.WriteLine( "The first number is bigger");
+            }
+            else {
+                Console.WriteLine("The second number is bigger");
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿using ConsoleApp1;
+
+class MyMain
+{
+    public static void Main(string []args)
+    {
+        TempClass tempClass = new TempClass();
+    }
+}
